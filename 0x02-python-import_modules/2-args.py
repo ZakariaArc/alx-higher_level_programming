77aa1da5937b9ccb79be1c22c796853d67f51e31
Zakaria <zakariaa123@hotmail.fr>
2023-10-05 17:07:35 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
     for i, arg in enumerate(args, start=1):
-            print("{}: {}".format(i, arg))
+        print("{}: {}".format(i, arg))
