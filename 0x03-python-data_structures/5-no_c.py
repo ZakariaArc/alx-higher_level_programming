66f2b9947be78246a10dc907754490a6dc28c988
Zakaria <zakariaa123@hotmail.fr>
2023-10-09 15:32:21 -0700

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    n_string = my_string.translate({ord(i): None for i in 'cC'})
-    return n_string
+    return ''.join(char for char in my_string if char.lower() != 'c')
