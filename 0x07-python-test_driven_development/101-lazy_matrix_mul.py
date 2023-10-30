ff6015399f080226a11a20f6ea3e2dd86a4e8dc8
Zakaria <zakariaa123@hotmail.fr>
2023-10-30 14:16:56 -0700

    pycodestyle fix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """
-This module multiply 2 matricies using numpy module
-"""
-import numpy
-"""
 Module for matrix multiplication using NumPy.
 """
 import numpy as np
@@ -16,7 +12,7 @@ def lazy_matrix_mul(m_a, m_b):
     Args:
         m_a (list of lists): The first matrix.
         m_b (list of lists): The second matrix.
-    
+
     Returns:
         return m_a * m_b
     """
