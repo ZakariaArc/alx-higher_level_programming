366136d11ed12ced40823479f3825ae5e70c2cd8
Zakaria <zakariaa123@hotmail.fr>
2023-11-06 04:46:46 -0800

    Task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """
-module containing a class MyInt that inherits from int.
+module that contains the class MyInt
 """
 
 
 class MyInt(int):
+    """Rebel version of an integer, perfect for opposite day!"""
+    def __new__(cls, *args, **kwargs):
+        """It create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
     def __eq__(self, other):
-        """inverts the == operator"""
-        return super().__ne__(other)
+        """from != to =="""
+        return int(self) != other
 
     def __ne__(self, other):
-        """inverts the != operator"""
-        return super().__eq__(other)
+        """from == to !="""
+        return int(self) == other
