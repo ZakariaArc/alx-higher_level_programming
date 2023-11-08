d49efd397b4bd04c32fab1dabd277a11005d3cfe
Zakaria <zakariaa123@hotmail.fr>
2023-11-07 17:20:28 -0800

    Task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-Module for appending a line of text after each occurrence of a specific string in a file.
+Module for appending a line of text after each occurrence
+of a specific string in a file.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text to a file after each line containing a specific string.
+    Inserts a line of text to a file after each line
+    containing a specific string.
 
     Args:
         filename (str): The name of the file.
-        search_string (str): The specific string to search for in each line.
-        new_string (str): The line of text to insert after each occurrence of search_string.
+        search_string (str): The specific string to
+        search for in each line.
+        new_string (str): The line of text to insert
+        after each occurrence of search_string.
     """
     with open(filename, mode='r', encoding='utf-8') as file:
         lines = file.readlines()
