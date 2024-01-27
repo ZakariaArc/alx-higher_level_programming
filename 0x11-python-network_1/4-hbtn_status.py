35682d1ceae2fe76fbab68f07c5e755e15b1a253
ZakariaArc <zakariaa123@hotmail.fr>
2024-01-27 06:03:32 -0800

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """fetches https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
